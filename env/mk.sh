0a770d8f8161b0044a7ab090e3d2f7296edb86da
honyax <honyax@gmail.com>
2020-09-22 03:12:17 +0900

    念の為kernel.cが無い場合＆kernel.sがある場合＆ない場合のビルド処理を記述

--- env/mk.sh
+++ env/mk.sh
@@ -1,5 +1,20 @@
-gcc.exe -c kernel.c
-objconv.exe -fnasm kernel.o kernel_base.tmp.s
-nasm.exe kernel.tmp.s -o kernel.bin -l kernel.lst
-nasm.exe boot.s -o boot.bin -l boot.lst
-cat boot.bin kernel.bin > boot.img
+if [ -e kernel.c ]; then
+  echo "kernel.c exists."
+  nasm.exe boot.s -o boot.bin -l boot.lst
+  gcc.exe -c kernel.c
+  objconv.exe -fnasm kernel.o kernel_base.tmp.s
+  nasm.exe kernel.tmp.s -o kernel.bin -l kernel.lst
+  cat boot.bin kernel.bin > boot.img
+
+elif [ -e kernel.s ]; then
+  echo "kernel.s exists."
+  nasm.exe boot.s -o boot.bin -l boot.lst
+  nasm.exe kernel.s -o kernel.bin -l kernel.lst
+  cat boot.bin kernel.bin > boot.img
+
+else
+  echo "kernel not exists."
+  nasm.exe boot.s -o boot.img -l boot.lst
+
+fi
+
