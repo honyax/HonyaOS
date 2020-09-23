4cddea701f2a3265120e517957f26f12844df1e3
honyax <honyax@gmail.com>
2020-09-23 23:55:01 +0900

    kernelのアセンブリ出力方法を変更。header/footerでなくtemplateファイルを使う方式にした。

--- env/convert.sh
+++ env/convert.sh
@@ -1,22 +1,33 @@
-if [ $# != 3 ]; then echo "convert.sh input_file header_file footer_file"; exit 1; fi
+if [ $# != 2 ]; then echo "convert.sh input_file template_file"; exit 1; fi
 if [ ! -e $1 ]; then echo $1 not exists; exit 1; fi
 if [ ! -e $2 ]; then echo $2 not exists; exit 1; fi
-if [ ! -e $3 ]; then echo $3 not exists; exit 1; fi
 INPUT_FILE=$1
-HEADER_FILE=$2
-FOOTER_FILE=$3
-
-cat $HEADER_FILE
+TEMPLATE_FILE=$2
 
 IFS=''
-cat $INPUT_FILE | while read line
+
+# テンプレートファイルを出力
+cat $TEMPLATE_FILE | while read template_line
 do
-  if [[ ${line} =~ ^global.* ]]; then echo -n ";"; fi
-  if [[ ${line} =~ ^SECTION.*rdata.* ]]; then exit 0; fi
-  line=${line//noexecute/}
-  line=${line//execute/}
-  echo ${line}
+
+  # テンプレートファイルの中の {KERNEL_SOURCECODE} の部分を入力ファイルで置き換え
+  if [[ ${template_line} =~ ^\{KERNEL_SOURCECODE\} ]]; then
+    echo
+    echo "; REPLACE {KERNEL_SOURCECODE}"
+    echo
+    
+    cat $INPUT_FILE | while read line
+    do
+      if [[ ${line} =~ ^global.* ]]; then echo -n ";"; fi
+      if [[ ${line} =~ ^SECTION.*rdata.* ]]; then exit 0; fi
+      line=${line//noexecute/}
+      line=${line//execute/}
+      echo ${line}
+    done
+  else
+    echo ${template_line}
+  fi
+
 done
 
-cat $FOOTER_FILE
 
--- env/mk.sh
+++ env/mk.sh
@@ -1,19 +1,25 @@
 if [ -e kernel.c ]; then
+
+  # kernel.c があればコンパイル＆NASMを生成してアセンブル＆結合
   echo "kernel.c exists."
   nasm.exe boot.s -o boot.bin -l boot.lst
   gcc.exe -c kernel.c
   objconv.exe -fnasm kernel.o kernel_base.tmp.s
-  convert.sh kernel_base.tmp.s kernel_header.s kernel_footer.s > kernel.tmp.s
+  convert.sh kernel_base.tmp.s kernel_template.s > kernel.tmp.s
   nasm.exe kernel.tmp.s -o kernel.bin -l kernel.lst
   cat boot.bin kernel.bin > boot.img
 
 elif [ -e kernel.s ]; then
+
+  # kernel.cが無い＆kernel.sがある場合はそのままアセンブル＆結合
   echo "kernel.s exists."
   nasm.exe boot.s -o boot.bin -l boot.lst
   nasm.exe kernel.s -o kernel.bin -l kernel.lst
   cat boot.bin kernel.bin > boot.img
 
 else
+
+  # kernelがない場合はそのままboot.sをアセンブル
   echo "kernel not exists."
   nasm.exe boot.s -o boot.img -l boot.lst
 
