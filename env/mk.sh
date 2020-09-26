e62e3552832f2e579aa75ddba456d525f22331f7
honyax <honyax@gmail.com>
2020-09-27 00:24:46 +0900

    もはやkernel.sの存在意義がなくなったので削除

--- env/mk.sh
+++ env/mk.sh
@@ -3,21 +3,13 @@ if [ -e kernel.c ]; then
   # kernel.c があればコンパイル＆NASMを生成してアセンブル＆結合
   echo "kernel.c exists."
   nasm.exe boot.s -o boot.bin -l boot.lst
-  rm kernel.tmp.c
+  rm -f kernel.tmp.c
   cat *.c > kernel.tmp.c
   gcc.exe -c kernel.tmp.c
-  rm kernel.tmp.c
+  rm -f kernel.tmp.c
   objconv.exe -fnasm kernel.tmp.o kernel_base.tmp.s
   convert.sh kernel_base.tmp.s kernel_template.s > kernel.tmp.s
   nasm.exe kernel.tmp.s -o kernel.bin -l kernel.lst
-  cat boot.bin kernel.bin > boot.img
-
-elif [ -e kernel.s ]; then
-
-  # kernel.cが無い＆kernel.sがある場合はそのままアセンブル＆結合
-  echo "kernel.s exists."
-  nasm.exe boot.s -o boot.bin -l boot.lst
-  nasm.exe kernel.s -o kernel.bin -l kernel.lst
   cat boot.bin kernel.bin > boot.img
 
 else
--- src/kernel.c
+++ src/kernel.c
@@ -4,9 +4,6 @@ int kernel_main() {
     draw_char(0, 0, 0x010F, 'A');
     draw_char(1, 0, 0x010F, 'B');
     draw_char(2, 0, 0x010F, 'C');
-    draw_char(0, 1, 0x0402, '0');
-    draw_char(1, 1, 0x0212, '1');
-    draw_char(2, 1, 0x0212, '_');
     int aaa = hemo(12345678, 5678, 78, 76543210, 7654, 76);
     fuga(aaa);
     return aaa;
