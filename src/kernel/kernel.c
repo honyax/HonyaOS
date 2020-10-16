9c0bc633f46d19e7e32c907e0795537166bddee3
honyax <honyax@gmail.com>
2020-10-16 20:53:36 +0900

    セクション初期化処理を切り出し

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -10,10 +10,12 @@ extern unsigned int _bss_start;
 extern unsigned int _bss_end;
 
 void write_bios_font_address();
+void init_sections();
 
 int kernel_main() {
 
     write_bios_font_address();
+    init_sections();
 
     int aaa = _asm_sample();
 
@@ -39,19 +41,6 @@ int kernel_main() {
     sprintf(test_txt, "This is %d, 0x%x, 0x%X, string:%s.", 100, 0x12AB, 0x34CD, "string parameter");
     draw_text(16, 160, test_txt, 0x55);
 
-    char addr_txt[128];
-    unsigned int _TEXT_START    = ( unsigned int )&_text_start;
-    unsigned int _TEXT_END      = ( unsigned int )&_text_end;
-    unsigned int _RODATA_START  = ( unsigned int )&_rodata_start;
-    unsigned int _RODATA_END    = ( unsigned int )&_rodata_end;
-    unsigned int _DATA_START    = ( unsigned int )&_data_start;
-    unsigned int _DATA_END      = ( unsigned int )&_data_end;
-    unsigned int _BSS_START     = ( unsigned int )&_bss_start;
-    unsigned int _BSS_END       = ( unsigned int )&_bss_end;
-    sprintf(addr_txt, "text:%X - %X rodata:%X - %X data:%X - %X bss:%X - %X",
-            _TEXT_START, _TEXT_END, _RODATA_START, _RODATA_END, _DATA_START, _DATA_END, _BSS_START, _BSS_END);
-    draw_text(16, 240, addr_txt, 0x33);
-
     for (;;) ;
 
     return 0;
@@ -66,4 +55,23 @@ void write_bios_font_address()
     font_addr += *font_offset;
     unsigned int *param_font_addr = (unsigned int *)PARAM_FONT_ADR;
     *param_font_addr = font_addr;
+}
+
+void init_sections()
+{
+    char addr_txt[128];
+    unsigned int _TEXT_START    = ( unsigned int )&_text_start;
+    unsigned int _TEXT_END      = ( unsigned int )&_text_end;
+    unsigned int _RODATA_START  = ( unsigned int )&_rodata_start;
+    unsigned int _RODATA_END    = ( unsigned int )&_rodata_end;
+    unsigned int _DATA_START    = ( unsigned int )&_data_start;
+    unsigned int _DATA_END      = ( unsigned int )&_data_end;
+    unsigned int _BSS_START     = ( unsigned int )&_bss_start;
+    unsigned int _BSS_END       = ( unsigned int )&_bss_end;
+    sprintf(addr_txt, "text:%X - %X rodata:%X - %X data:%X - %X bss:%X - %X",
+            _TEXT_START, _TEXT_END, _RODATA_START, _RODATA_END, _DATA_START, _DATA_END, _BSS_START, _BSS_END);
+    draw_text(16, 480, addr_txt, 0x33);
+
+    // BSSを初期化
+    hmemset((void *)_BSS_START, 0x00, _BSS_END - _BSS_START);
 }
