1a6384fa597f4da17803e845133b7fe5201b0825
honyax <honyax@gmail.com>
2020-10-18 04:00:05 +0900

    GDT初期化処理を実装

--- src/kernel/asm.s
+++ src/kernel/asm.s
@@ -1,6 +1,12 @@
 .global _load_idt
+.global _load_gdt
 .extern idtr
+.extern gdtr
 
 _load_idt:
     lidt    idtr
+    ret
+
+_load_gdt:
+    lgdt    gdtr
     ret
--- src/kernel/descriptor.c
+++ src/kernel/descriptor.c
@@ -10,6 +10,7 @@
 
 // IDTは256個
 #define     NUM_IDT     256
+// ゲートディスクリプタ
 typedef struct
 {
     unsigned short  base_lo;
@@ -30,25 +31,92 @@ IDTR idtr;
 
 void _load_idt();
 
-void setup_gate_descriptor(int num, int base, unsigned short selector, unsigned char flags)
+void setup_gate_descriptor(int index, int base, unsigned short selector, unsigned char flags)
 {
-    idt[num].base_lo = (unsigned short)(base & 0x0000FFFF);
-    idt[num].selector = selector;
-    idt[num].reserved = 0;
-    idt[num].flags = flags;
-    idt[num].base_hi = (unsigned short)((base & 0xFFFF0000) >> 16);
+    idt[index].base_lo = (unsigned short)(base & 0x0000FFFF);
+    idt[index].selector = selector;
+    idt[index].reserved = 0;
+    idt[index].flags = flags;
+    idt[index].base_hi = (unsigned short)((base & 0xFFFF0000) >> 16);
 }
 
-void setup_interrupt_gate(int num, void *handler)
+void setup_interrupt_gate(int index, void *handler)
 {
-    setup_gate_descriptor(num, (int)handler, IDT_INT_SELECTOR,
+    setup_gate_descriptor(index, (int)handler, IDT_INT_SELECTOR,
             IDT_FLAGS_SEGMENT_PRESENT | IDT_FLAGS_DPL_0 | IDT_FLAGS_INTGATE);
+}
+
+// GDTは十分に用意しておく
+#define     NUM_GDT     1024
+// セグメントディスクリプタ
+typedef struct
+{
+    unsigned short  limit_lo;
+    unsigned short  base_lo;
+    unsigned char   base_mid;
+    unsigned char   flags;
+    unsigned char   limit_hi_flags;
+    unsigned char   base_hi;
+} SEGMENT_DESCRIPTOR;
+SEGMENT_DESCRIPTOR gdt[NUM_GDT];
+
+// GDTRを定義
+typedef struct
+{
+    unsigned short      size;
+    SEGMENT_DESCRIPTOR* base;
+} GDTR;
+GDTR gdtr;
+
+void _load_gdt();
+
+// Segment Descriptor定義値
+#define GDT_IDX_NULL        0
+#define GDT_IDX_CODE        1
+#define GDT_IDX_DATA        2
+#define GDT_IDX_TEMP        3
+#define GDT_IDX_TASK_CODE   3
+#define GDT_IDX_TASK_DATA   4
+#define GDT_IDX_KTSS        5
+
+#define GDT_FLAGS_DEFAULT   0xC082
+#define GDT_FLAGS_DPL_0     0x0000
+#define GDT_FLAGS_DPL_3     0x0060
+#define GDT_FLAGS_TYPE_SYS  0x0000
+#define GDT_FLAGS_TYPE_CODE 0x0018
+#define GDT_FLAGS_TYPE_DATA 0x0010
+#define GDT_NULL_LIMIT      0x00000
+#define GDT_NULL_BASE       0x00000000
+#define GDT_NULL_FLAGS      0x0000
+#define GDT_CODE_LIMIT      0xFFFFF
+#define GDT_CODE_BASE       0x00000000
+#define GDT_CODE_FLAGS      GDT_FLAGS_DEFAULT | GDT_FLAGS_DPL_0 | GDT_FLAGS_TYPE_CODE
+#define GDT_DATA_LIMIT      0xFFFFF
+#define GDT_DATA_BASE       0x00000000
+#define GDT_DATA_FLAGS      GDT_FLAGS_DEFAULT | GDT_FLAGS_DPL_0 | GDT_FLAGS_TYPE_DATA
+
+void setup_segment_descriptor(int index, int limit, int base, unsigned short flags)
+{
+    gdt[index].limit_lo = (unsigned short)(limit & 0x0000FFFF);
+    gdt[index].base_lo = (unsigned short)(base & 0x0000FFFF);
+    gdt[index].base_mid = (unsigned char)((base & 0x00FF0000) >> 16);
+    gdt[index].flags = (unsigned char)(flags & 0x00FF);
+    gdt[index].limit_hi_flags = (unsigned char)((limit & 0x00F0000) >> 16 | (flags & 0xF000) >> 8);
+    gdt[index].base_hi = (unsigned char)((base & 0xFF000000) >> 24);
 }
 
 void init_descriptor()
 {
+    for (int i = 0; i < NUM_GDT; i++) {
+        setup_segment_descriptor(i, GDT_NULL_LIMIT, GDT_NULL_BASE, GDT_NULL_FLAGS);
+    }
+    setup_segment_descriptor(GDT_IDX_CODE, GDT_CODE_LIMIT, GDT_CODE_BASE, GDT_CODE_FLAGS);
+    setup_segment_descriptor(GDT_IDX_DATA, GDT_DATA_LIMIT, GDT_DATA_BASE, GDT_DATA_FLAGS);
+    gdtr.size = NUM_GDT * sizeof(SEGMENT_DESCRIPTOR);
+    gdtr.base = gdt;
+    _load_gdt();
+
     idtr.size = NUM_IDT * sizeof(GATE_DESCRIPTOR);
     idtr.base = idt;
-
     _load_idt();
 }
