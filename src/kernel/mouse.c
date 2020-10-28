5536b5917d570a78c54a8b1f8221d1b542fec48a
honyax <honyax@gmail.com>
2020-10-29 01:50:17 +0900

    MOUSE_DATA構造体を作って色々整理

--- src/kernel/mouse.c
+++ src/kernel/mouse.c
@@ -1,51 +1,65 @@
 #include "define.h"
 
-#define MOUSE_INPUT_BUF_SIZE    50
+#define MOUSE_INPUT_BUF_SIZE    128     // マウスのインプットデータを格納するバッファのサイズ
 
 FIFO32 mouse_input_data;
 int mouse_input_buff[MOUSE_INPUT_BUF_SIZE];
 
-#define MOUSE_STATE_INIT    0   // 最初の0xFAが来る前の状態
-#define MOUSE_STATE_PHASE_1 1   // １バイト目のデータを待っている状態
-#define MOUSE_STATE_PHASE_2 2   // ２バイト目のデータを待っている状態
-#define MOUSE_STATE_PHASE_3 3   // ３バイト目のデータを待っている状態
-int mouse_state = MOUSE_STATE_INIT;
+#define MOUSE_STATE_INIT        0   // 最初の0xFAが来る前の状態
+#define MOUSE_STATE_PHASE_1     1   // １バイト目のデータを待っている状態
+#define MOUSE_STATE_PHASE_2     2   // ２バイト目のデータを待っている状態
+#define MOUSE_STATE_PHASE_3     3   // ３バイト目のデータを待っている状態
+
+typedef struct
+{
+    int x;
+    int y;
+    int button;
+    unsigned char buf[3];
+    unsigned char state;
+} MOUSE_DATA;
+MOUSE_DATA mouse_data;
 
 void init_mouse()
 {
     // FIFOバッファの初期化
     fifo32_init(&mouse_input_data, MOUSE_INPUT_BUF_SIZE, mouse_input_buff);
+
+    mouse_data.state = MOUSE_STATE_INIT;
 }
 
 int update_mouse()
 {
     int length = mouse_input_data.len;
     for (int i = 0; i < length; i++) {
-        int data = fifo32_get(&mouse_input_data);
+        unsigned char data = (unsigned char) fifo32_get(&mouse_input_data);
         int pos_x = 0;
-        switch (mouse_state) {
+        switch (mouse_data.state) {
             case MOUSE_STATE_INIT:
                 if (data == 0xfa) {
-                    mouse_state = MOUSE_STATE_PHASE_1;
+                    mouse_data.state = MOUSE_STATE_PHASE_1;
                 }
                 continue;
             case MOUSE_STATE_PHASE_1:
-                pos_x = 16;
-                mouse_state = MOUSE_STATE_PHASE_2;
+                // 正しいマウスデータの１バイト目か念の為チェック
+                if ((data & 0xC8) == 0x08) {
+                    mouse_data.buf[0] = data;
+                    mouse_data.state = MOUSE_STATE_PHASE_2;
+                }
                 break;
             case MOUSE_STATE_PHASE_2:
-                pos_x = 16 + 24;
-                mouse_state = MOUSE_STATE_PHASE_3;
+                mouse_data.buf[1] = data;
+                mouse_data.state = MOUSE_STATE_PHASE_3;
                 break;
             case MOUSE_STATE_PHASE_3:
-                pos_x = 16 + 24 * 2;
-                mouse_state = MOUSE_STATE_PHASE_1;
+                mouse_data.buf[2] = data;
+                mouse_data.state = MOUSE_STATE_PHASE_1;
+                char mouse_code[16];
+                sprintf(mouse_code, "%X %X %X", mouse_data.buf[0], mouse_data.buf[1], mouse_data.buf[2]);
+                draw_rect(16, 460, 64, 16, COL_BLACK);
+                draw_text(16, 460, mouse_code, COL_WHITE);
                 break;
         }
-        char mouse_code[4];
-        sprintf(mouse_code, "%X", data);
-        draw_rect(pos_x, 460, 16, 16, COL_BLACK);
-        draw_text(pos_x, 460, mouse_code, COL_WHITE);
     }
     return length;
 }
