d619936f9a8ff2c7154b6c382828cdb3958d66ae
honyax <honyax@gmail.com>
2020-10-29 01:37:17 +0900

    マウスの読み込み準備

--- src/kernel/mouse.c
+++ src/kernel/mouse.c
@@ -5,6 +5,12 @@
 FIFO32 mouse_input_data;
 int mouse_input_buff[MOUSE_INPUT_BUF_SIZE];
 
+#define MOUSE_STATE_INIT    0   // 最初の0xFAが来る前の状態
+#define MOUSE_STATE_PHASE_1 1   // １バイト目のデータを待っている状態
+#define MOUSE_STATE_PHASE_2 2   // ２バイト目のデータを待っている状態
+#define MOUSE_STATE_PHASE_3 3   // ３バイト目のデータを待っている状態
+int mouse_state = MOUSE_STATE_INIT;
+
 void init_mouse()
 {
     // FIFOバッファの初期化
@@ -16,8 +22,27 @@ int update_mouse()
     int length = mouse_input_data.len;
     for (int i = 0; i < length; i++) {
         int data = fifo32_get(&mouse_input_data);
+        int pos_x = 0;
+        switch (mouse_state) {
+            case MOUSE_STATE_INIT:
+                if (data == 0xfa) {
+                    mouse_state = MOUSE_STATE_PHASE_1;
+                }
+                continue;
+            case MOUSE_STATE_PHASE_1:
+                pos_x = 16;
+                mouse_state = MOUSE_STATE_PHASE_2;
+                break;
+            case MOUSE_STATE_PHASE_2:
+                pos_x = 16 + 24;
+                mouse_state = MOUSE_STATE_PHASE_3;
+                break;
+            case MOUSE_STATE_PHASE_3:
+                pos_x = 16 + 24 * 2;
+                mouse_state = MOUSE_STATE_PHASE_1;
+                break;
+        }
         char mouse_code[4];
-        int pos_x = 16 + 24 * mouse_input_data.pos_r;
         sprintf(mouse_code, "%X", data);
         draw_rect(pos_x, 460, 16, 16, COL_BLACK);
         draw_text(pos_x, 460, mouse_code, COL_WHITE);
