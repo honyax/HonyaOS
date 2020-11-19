9e9646556a88d9150618d04dc89d0f541e94cdd0
honyax <honyax@gmail.com>
2020-11-20 02:32:15 +0900

    システムコールの引数の名前をレジスタでなくした。多分こっちのがCの実装側は楽なはずなので。

--- src/kernel/syscall.c
+++ src/kernel/syscall.c
@@ -1,16 +1,16 @@
 #include "define.h"
 
 // 各システムコールの処理
-void syscall(int edi, int esi, int ebx, int edx, int ecx, int eax)
+void syscall(int sc_id, int param1, int param2, int param3, int param4, int param5)
 {
     // アプリケーションに渡す戻り値をresultに設定する
     int result = 0;
 
     char str[64];
 
-    switch (edi) {
+    switch (sc_id) {
         case 0xffff:
-            hsprintf(str, "%X %X %X %X %X %X", edi, esi, ebx, edx, ecx, eax);
+            hsprintf(str, "%X %X %X %X %X %X", sc_id, param1, param2, param3, param4, param5);
             draw_text(20, 520, str, COL_WHITE);
             result = 0x1234abcd;
             break;
@@ -20,7 +20,7 @@ void syscall(int edi, int esi, int ebx, int edx, int ecx, int eax)
     }
 
     // 戻り値をスタックに入れる
-    int* p_result = &eax + 1;
+    int* p_result = &param5 + 1;
     *p_result = result;
 
     return;
