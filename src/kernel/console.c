63cf25f0626fc72a767b41077b6b9636dc93ff95
honyax <honyax@gmail.com>
2020-12-19 14:29:50 +0900

    lsコマンドをひとまず実装。ファイルの名前を1行ずつ表示

--- src/kernel/console.c
+++ src/kernel/console.c
@@ -3,7 +3,7 @@
 #define TEXT_LENGTH 40  // 1280 / 4 / 8  = 40
 #define LINE_COUNT 8    // 1024 / 8 / 16 = 8
 
-static WINDOW* console_win;
+static WINDOW *console_win;
 
 typedef struct
 {
@@ -13,6 +13,20 @@ typedef struct
     int display_pos;
 } CONSOLE;
 static CONSOLE cons_data;
+
+typedef struct
+{
+    byte name[8];
+    byte ext[3];
+    byte type;
+    char reserve[10];
+    ushort time;
+    ushort date;
+    ushort clustno;
+    uint size;
+} FILEINFO;
+static FILEINFO *file_info;
+static int max_file_count;
 
 void refresh_console();
 void exec_command(char *line);
@@ -85,7 +99,7 @@ void println(const char *str)
 void init_console()
 {
     int w = param_screen_x / 4;
-    int h = param_screen_y / 8;
+    int h = param_screen_y / 4;
     int x = param_screen_x / 2 - (w / 2);
     int y = param_screen_y / 2 - (h  / 2);
     console_win = win_create(x, y, w, h);
@@ -95,6 +109,23 @@ void init_console()
     cons_data.current_line = 0;
     cons_data.current_pos = 1;
     cons_data.display_pos = 0;
+
+    // ファイルシステムヘッダの解析
+    byte* fs_pos = (byte*) FILESYSTEM_LOAD;
+
+    ushort fat_tbl_head_sector = fs_pos[0x0f] << 8 | fs_pos[0x0e];
+    byte fat_tbl_num = fs_pos[0x10];
+    ushort sector_per_fat_tbl = fs_pos[0x17] << 8 | fs_pos[0x16];
+    max_file_count = fs_pos[0x12] << 8 | fs_pos[0x11];
+
+#if 0
+    // FATテーブルの領域
+    char str[64];
+    hsprintf(str, "%d, %d, %d, %d", fat_tbl_head_sector, fat_tbl_num, sector_per_fat_tbl, max_file_count);
+    println(str);
+#endif
+    // FATテーブルをスキップした先頭が、ファイルの先頭
+    file_info = (FILEINFO *) &(fs_pos[SECT_SIZE * fat_tbl_head_sector + SECT_SIZE * sector_per_fat_tbl * fat_tbl_num]);
 }
 
 void update_console()
@@ -170,6 +201,35 @@ void exec_command(char *input)
         hsprintf(str, "%d, %d, %d, %d", total, used, free, count);
         println("total, used, free, count");
         println(str);
+    } else if (cmd_equals(input, "ls")) {
+        // ファイルリストを表示
+        
+        char fname[16];
+        int fname_index = 0;
+        for (int i = 0; i < max_file_count; i++) {
+            // nameの先頭が 0x00 の場合はそこで終了
+            if (file_info[i].name[0] == 0x00)
+                break;
+            // フィアルの場合はタイプが 0x20
+            if (file_info[i].type != 0x20)
+                continue;
+            
+            // ファイル名を出力
+            fname_index = 0;
+            for (int j = 0; j < 8; j++) {
+                if (file_info[i].name[j] != 0x20) {
+                    fname[fname_index++] = file_info[i].name[j];
+                }
+            }
+            if (file_info[i].ext[0] != 0x20) {
+                fname[fname_index++] = '.';
+                for (int j = 0; j < 3; j++) {
+                    fname[fname_index++] = file_info[i].ext[j];
+                }
+            }
+            fname[fname_index] = 0;
+            println(fname);
+        }
     } else {
         println("Bad Command");
     }
