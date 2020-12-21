5f0e5d452acfbc4348830a943734c84e4755d441
honyax <honyax@gmail.com>
2020-12-22 01:10:43 +0900

    string.cに、文字列比較（hstrncmp, hstrcmp）と文字のインデックス取得（hstrindexof）関数を実装

--- src/lib/define.h
+++ src/lib/define.h
@@ -1,5 +1,8 @@
 // string.c
 int hstrlen(const char *str);
+int hstrncmp(const char *s1, const char *s2, int len);
+int hstrcmp(const char *s1, const char *s2);
+int hstrindexof(const char *s, char c);
 
 // sprintf.c
 int hsprintf (char *str, const char *fmt, ...);
--- src/lib/string.c
+++ src/lib/string.c
@@ -2,9 +2,44 @@ int hstrlen(const char *str)
 {
 	// 文字列の長さをチェック。最大で1024文字にしておく
 	for (int i = 0; i < 1024; i++) {
-		if (str[i] == 0)
+		if (str[i] == 0) {
 			return i;
+        }
 	}
 	
 	return -1;
+}
+
+int hstrncmp(const char *s1, const char *s2, int len)
+{
+    for (int i = 0; i < len; i++) {
+        if (s1[i] != s2[i]) {
+            return s1[i] - s2[i];
+        }
+    }
+
+    return 0;
+}
+
+int hstrcmp(const char *s1, const char *s2)
+{
+    int len = hstrlen(s1);
+    int len2 = hstrlen(s2);
+
+    if (len != len2) {
+        return len - len2;
+    }
+
+    return hstrncmp(s1, s2, len);
+}
+
+int hstrindexof(const char *s, char c)
+{
+    int len = hstrlen(s);
+    for (int i = 0; i < len; i++) {
+        if (s[i] == c)
+            return i;
+    }
+
+    return -1;
 }
