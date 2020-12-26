b9fe0d79b4936a4d3d547d2b9d4d1ba4b639c9d1
honyax <honyax@gmail.com>
2020-12-26 21:28:15 +0900

    アプリケーション共通のdefineをkernelからlibに移動させた。

--- src/kernel/define.h
+++ src/kernel/define.h
@@ -37,50 +37,6 @@
 
 #define     VBE_MODE                0x107           // 画面モード（1280 x 1024 x 8bitカラー）
 
-// 共通
-#define     NULL            0
-#define     TRUE            1
-#define     FALSE           0
-typedef     unsigned char   bool;
-typedef     unsigned char   byte;
-typedef     unsigned short  ushort;
-typedef     unsigned int    uint;
-
-// カラーコード
-#define     COL_000000      0
-#define     COL_0000A8      1
-#define     COL_00A800      2
-#define     COL_00A8A8      3
-#define     COL_A80000      4
-#define     COL_A800A8      5
-#define     COL_A85400      6
-#define     COL_A8A8A8      7
-#define     COL_545454      8
-#define     COL_5454FC      9
-#define     COL_54FC54      10
-#define     COL_54FCFC      11
-#define     COL_FC5454      12
-#define     COL_FC54FC      13
-#define     COL_FCFC54      14
-#define     COL_FCFCFC      15
-#define     COL_BLACK       COL_000000
-#define     COL_DARKBLUE    COL_0000A8
-#define     COL_DARKGREEN   COL_00A800
-#define     COL_DARKCYAN    COL_00A8A8
-#define     COL_DARKRED     COL_A80000
-#define     COL_DARKPURPLE  COL_A800A8
-#define     COL_BROWN       COL_A85400
-#define     COL_GREY        COL_A8A8A8
-#define     COL_DARKGREY    COL_545454
-#define     COL_BLUE        COL_5454FC
-#define     COL_GREEN       COL_54FC54
-#define     COL_CYAN        COL_54FCFC
-#define     COL_RED         COL_FC5454
-#define     COL_PURPLE      COL_FC54FC
-#define     COL_YELLOW      COL_FCFC54
-#define     COL_WHITE       COL_FCFCFC
-#define     COL_NONE        255
-
 // Global Segment Descriptor定義値
 #define GDT_IDX_NULL        0
 #define GDT_IDX_CODE        1
--- src/lib/define.h
+++ src/lib/define.h
@@ -1,3 +1,47 @@
+// 共通
+#define     NULL            0
+#define     TRUE            1
+#define     FALSE           0
+typedef     unsigned char   bool;
+typedef     unsigned char   byte;
+typedef     unsigned short  ushort;
+typedef     unsigned int    uint;
+
+// カラーコード
+#define     COL_000000      0
+#define     COL_0000A8      1
+#define     COL_00A800      2
+#define     COL_00A8A8      3
+#define     COL_A80000      4
+#define     COL_A800A8      5
+#define     COL_A85400      6
+#define     COL_A8A8A8      7
+#define     COL_545454      8
+#define     COL_5454FC      9
+#define     COL_54FC54      10
+#define     COL_54FCFC      11
+#define     COL_FC5454      12
+#define     COL_FC54FC      13
+#define     COL_FCFC54      14
+#define     COL_FCFCFC      15
+#define     COL_BLACK       COL_000000
+#define     COL_DARKBLUE    COL_0000A8
+#define     COL_DARKGREEN   COL_00A800
+#define     COL_DARKCYAN    COL_00A8A8
+#define     COL_DARKRED     COL_A80000
+#define     COL_DARKPURPLE  COL_A800A8
+#define     COL_BROWN       COL_A85400
+#define     COL_GREY        COL_A8A8A8
+#define     COL_DARKGREY    COL_545454
+#define     COL_BLUE        COL_5454FC
+#define     COL_GREEN       COL_54FC54
+#define     COL_CYAN        COL_54FCFC
+#define     COL_RED         COL_FC5454
+#define     COL_PURPLE      COL_FC54FC
+#define     COL_YELLOW      COL_FCFC54
+#define     COL_WHITE       COL_FCFCFC
+#define     COL_NONE        255
+
 // string.c
 int hstrlen(const char *str);
 int hstrncmp(const char *s1, const char *s2, int len);
