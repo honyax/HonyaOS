2552aebd6215728b0a7815803e24b7d7c06c72cc
honyax <honyax@gmail.com>
2020-12-27 12:57:19 +0900

    feat: _sc_bg_draw_textを、ちゃんと指定した位置に描画するようにした。

--- src/kernel/syscall.c
+++ src/kernel/syscall.c
@@ -19,9 +19,7 @@ void syscall(int sc_id, int param1, int param2, int param3, int param4, int para
             break;
 
         case 2:
-            hsprintf(str, "%d %d %d 0x%X %s %d", sc_id, param1, param2, param3, (char *)param3, param4);
-            bg_draw_text(20, 680, str, COL_WHITE);
-            result = 0x1234abcd;
+            bg_draw_text(param1, param2, (char *)param3, param4);
             break;
     }
 
--- src/tasks/example.c
+++ src/tasks/example.c
@@ -1,9 +1,10 @@
 #include "../syscall/define.h"
+#include "../lib/define.h"
 
 void main()
 {
     _sc_example(0x100, 0x200, 0x500, 0x1000, 0x10000);
-    _sc_bg_draw_text(80, 700, (int) ("hogehoge"), 10);
+    _sc_bg_draw_text(80, 700, (int) ("hogehoge"), COL_WHITE);
 
     for (;;) {
     }
