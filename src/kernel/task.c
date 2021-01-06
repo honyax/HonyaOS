772c2692059681265ce72486fc09ab7c47cb1491
honyax <honyax@gmail.com>
2021-01-07 03:11:55 +0900

    タスクにstateの概念を設けた。また、タスクの構造体としてTASK_INFORMATIONを作成し、TSSとstateを内包するようにした。

--- src/kernel/task.c
+++ src/kernel/task.c
@@ -5,10 +5,20 @@
 #define TASK_MAX_NUM    64              // タスクは最大64
 #define TASK_INDEX_ROOT 0
 
+typedef enum {
+    TASK_STATE_NONE = 1,
+    TASK_STATE_WORK,
+    TASK_STATE_SLEEP,
+} TASK_STATE;
+
+typedef struct {
+    TASK_STATE state;
+    TSS tss;
+} TASK_INFORMATION;
+
 // Task State Segment
 // Index:0 はRoot Task
-static TSS task[TASK_MAX_NUM];
-static int current_task_num;
+static TASK_INFORMATION task[TASK_MAX_NUM];
 
 // 実行中のタスクのIndex（0:root、1～:task）
 static int running_task_index;
@@ -18,13 +28,18 @@ void task_c_main();
 
 void init_task()
 {
+    // タスクの状態を初期化
+    for (int i = 0; i < TASK_MAX_NUM; i++) {
+        task[i].state= TASK_STATE_NONE;
+    }
+
     // 初期のrootタスクを生成
-    task[TASK_INDEX_ROOT].cr3 = KERNEL_PAGE_DIR;
-    task[TASK_INDEX_ROOT].ldtr = GDT_IDX_LDT * 8;
-    task[TASK_INDEX_ROOT].iomap = TASK_IOMAP;
-    create_root_task_descriptor(&task[TASK_INDEX_ROOT]);
+    task[TASK_INDEX_ROOT].tss.cr3 = KERNEL_PAGE_DIR;
+    task[TASK_INDEX_ROOT].tss.ldtr = GDT_IDX_LDT * 8;
+    task[TASK_INDEX_ROOT].tss.iomap = TASK_IOMAP;
+    task[TASK_INDEX_ROOT].state = TASK_STATE_WORK;
+    create_root_task_descriptor(&task[TASK_INDEX_ROOT].tss);
     _load_tr(4 * 8);
-    current_task_num = 1;
 
     add_task(&task_b_main);
     add_task(&task_c_main);
@@ -38,43 +53,59 @@ void add_task(void *addr)
     int page_addr = (int) hmalloc(8 * 1024);
     init_task_paging((uint *)page_addr, (uint)addr);
 
-    int index = current_task_num;
-    task[index];
-    task[index].cr3 = page_addr;
-    task[index].ldtr = GDT_IDX_LDT * 8;
-    task[index].iomap = TASK_IOMAP;
-    create_task_descriptor(&task[index], index);
+    int index;
+    for (index = 0; index < TASK_MAX_NUM; index++) {
+        if (task[index].state == TASK_STATE_NONE) {
+            break;
+        }
+    }
+    if (index == TASK_MAX_NUM) {
+        // タスクの空きがない場合は無処理
+        return;
+    }
+
+    task[index].tss.cr3 = page_addr;
+    task[index].tss.ldtr = GDT_IDX_LDT * 8;
+    task[index].tss.iomap = TASK_IOMAP;
+    task[index].state = TASK_STATE_WORK;
+    create_task_descriptor(&task[index].tss, index);
 
     // スタック領域の確保（前半32KBは権限0、後半32KBは権限3の時に使用する）
     int task_esp = ((int)hmalloc(64 * 1024)) + 64 * 1024;
     // 権限0で動作するときのss, espを設定
-    task[index].ss0 = GDT_IDX_DATA * 8;
-    task[index].esp0 = task_esp - 32 * 1024;
-    task[index].eip = (int) addr;
-    task[index].eflags = TASK_EFLAGS;
-    task[index].eax = 0;
-    task[index].ecx = 0;
-    task[index].edx = 0;
-    task[index].ebx = 0;
-    task[index].esp = task_esp;
-    task[index].ebp = 0;
-    task[index].esi = 0;
-    task[index].edi = 0;
+    task[index].tss.ss0 = GDT_IDX_DATA * 8;
+    task[index].tss.esp0 = task_esp - 32 * 1024;
+    task[index].tss.eip = (int) addr;
+    task[index].tss.eflags = TASK_EFLAGS;
+    task[index].tss.eax = 0;
+    task[index].tss.ecx = 0;
+    task[index].tss.edx = 0;
+    task[index].tss.ebx = 0;
+    task[index].tss.esp = task_esp;
+    task[index].tss.ebp = 0;
+    task[index].tss.esi = 0;
+    task[index].tss.edi = 0;
     int task_cs = (LDT_IDX_ROOT_CS + index * 2) * 8 | 4 | 3;
     int task_ds = (LDT_IDX_ROOT_DS + index * 2) * 8 | 4 | 3;
-    task[index].es = task_ds;
-    task[index].cs = task_cs;
-    task[index].ss = task_ds;
-    task[index].ds = task_ds;
-    task[index].fs = task_ds;
-    task[index].gs = task_ds;
-
-    current_task_num++;
+    task[index].tss.es = task_ds;
+    task[index].tss.cs = task_cs;
+    task[index].tss.ss = task_ds;
+    task[index].tss.ds = task_ds;
+    task[index].tss.fs = task_ds;
+    task[index].tss.gs = task_ds;
 }
 
 void task_switch() {
     // task_indexは、現在動作中のタスクのIndexを繰り返し
-    running_task_index = (running_task_index + 1) % current_task_num;
+    running_task_index++;
+    for (; running_task_index < TASK_MAX_NUM; running_task_index++) {
+        if (task[running_task_index].state == TASK_STATE_WORK) {
+            break;
+        }
+    }
+    if (running_task_index == TASK_MAX_NUM) {
+        running_task_index = 0;
+    }
     _farjmp(0, (running_task_index + GDT_IDX_ROOT_TSS) * 8);
 }
 
