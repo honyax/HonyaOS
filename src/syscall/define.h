ee633477248d07a54737c8d86a8763ed882ebd60
honyax <honyax@gmail.com>
2021-01-09 01:44:28 +0900

    関数の位置が気になったので調整

--- src/syscall/define.h
+++ src/syscall/define.h
@@ -15,6 +15,6 @@ int _sc_example(int param1, int param2, int param3, int param4, int param5);
 void _sc_putchar(int c);
 void _sc_bg_draw_text(int x, int y, char *text, int color);
 int _sc_get_current_time();
+void _sc_sleep(int milliseconds);
 int _sc_win_create(RECT *rect);
 int _sc_win_draw_rect(int win_handle, RECT *rect, int color);
-void _sc_sleep(int milliseconds);
