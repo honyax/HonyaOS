8e8e0021f73236d8a8eaf6b48aa9ab70c8d59711
honyax <honyax@gmail.com>
2021-01-16 00:35:23 +0900

    fix: 平方根算出処理のバグ（無限ループ）を修正

--- src/lib/math.c
+++ src/lib/math.c
@@ -8,8 +8,10 @@ int hsqrt(int s)
 
     int x = s / 2;
     int last_x = 0;
+    int last_x_2 = 0;
 
-    while (x != 0 && x != last_x) {
+    for (int i = 0; x != 0 && x != last_x && x != last_x_2 && i < 100; i++) {
+        last_x_2 = last_x;
         last_x = x;
         x = (x + s / x) / 2;
     }
