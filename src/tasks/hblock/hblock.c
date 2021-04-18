9028db14df66c96423fd50f02d7122c1b41cca26
honyax <honyax@gmail.com>
2021-04-18 15:53:05 +0900

    feat: ボールに速度を持たせて、画面端に到達したらループさせる

--- src/tasks/hblock/hblock.c
+++ src/tasks/hblock/hblock.c
@@ -13,14 +13,38 @@ void main()
     rect.h = BOARD_HEIGHT;
     int win_handle = _sc_win_create(&rect);
 
-    RECT ball;
-    rect.x = 100;
-    rect.y = 100;
-    rect.w = 4;
-    rect.h = 4;
-    _sc_win_draw_rect(win_handle, &rect, COL_WHITE);
+    // 位置と速度は、ドットを100倍したものにする
+    VECTOR p;
+    VECTOR v;
+    p.x = 100 * 100;
+    p.y = 100 * 100;
+    v.x = 3 * 100;
+    v.y = -3 * 100;
 
-    for (;;) {
+    RECT ball;
+    ball.x = p.x / 100;
+    ball.y = p.y / 100;
+    ball.w = 4;
+    ball.h = 4;
+    _sc_win_draw_rect(win_handle, &ball, COL_WHITE);
+
+    for (int i = 0; ; i++) {
+        p.x += v.x;
+        p.y += v.y;
+        if (p.x > BOARD_WIDTH * 100) {
+            p.x = 0;
+        } else if (p.x < 0) {
+            p.x = BOARD_WIDTH * 100;
+        }
+        if (p.y > BOARD_HEIGHT * 100) {
+            p.y = 0;
+        } else if (p.y < 0) {
+            p.y = BOARD_HEIGHT * 100;
+        }
+        _sc_win_draw_rect(win_handle, &ball, COL_BLACK);
+        ball.x = p.x / 100;
+        ball.y = p.y / 100;
+        _sc_win_draw_rect(win_handle, &ball, COL_WHITE);
         _sc_sleep(50);
     }
 }
@@ -30,8 +54,8 @@ void main()
 TODO:
 15:30 ★ボードを作る
 15:40 ★ボールを生成する
-15:50 ☆ボールに速度を持たせる
-16:00 ☆ボールを速度方向に毎フレーム動かす
+15:50 ★ボールに速度を持たせる
+16:00 ★ボールを速度方向に毎フレーム動かす
 16:10 ☆ボールが壁にあたったら速度を反射する
 16:20 ☆自キャラのバーを作る
 16:30 ☆マウスの位置を検知する
